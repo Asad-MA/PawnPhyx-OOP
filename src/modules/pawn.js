8c55b1d6edd8159e76ae477b3a82823f6589d1fc
Asad@dev <100404439+Asad-MA@users.noreply.github.com>
2023-02-21 19:12:07 +0500

    Pawn-PhyX

--- src/modules/pawn.js
+++ src/modules/pawn.js
@@ -1,6 +1,22 @@
 class Pawn {
-    constructor ( color ) {
+    constructor ( color  ) {
         this.color = color;
+        this.player = color;
+        this.state = 'INACTIVE';
+        this.steps = -1;
+        this.position = {
+            x: 0,
+            y: 0
+        }
+
+    }
+
+    canMove(){
+
+    }
+
+    canKill(){
+
     }
 }
 
