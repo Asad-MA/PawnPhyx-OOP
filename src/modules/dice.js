6dcda3d2ffec7bbd92de0491589545033385eab7
mash-mixture <61063539+mash-mixture@users.noreply.github.com>
2023-02-23 07:32:58 +0500

    mechnix

---
+++ src/modules/dice.js
@@ -0,0 +1,7 @@
+class Dice {
+    constructor ( ) {
+
+    }
+}
+
+export default Dice;
---
+++ src/modules/player.js
@@ -0,0 +1,13 @@
+class Player {
+    constructor(player, id) {
+        this.player = player;
+        this.id = id;
+
+    }
+
+    hasFreePawns() {
+
+    }
+}
+
+export default Player;
